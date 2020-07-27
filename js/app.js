04a9788a68b6c1a6efcdb1cd37a54a8b9f796023
michaelj916 <46703736+michaelj916@users.noreply.github.com>
2020-07-27 13:00:36 -0700

    created portfolio

---
+++ js/app.js
@@ -0,0 +1,42 @@
+var $cursor = $('.cursor');
+var $cursorHover = $('.cursorHover');
+
+// move effect
+function cursorMover(e) {
+    gsap.to($cursor, {
+        x: e.clientX,
+        y: e.clientY,
+        stagger: 0.002,
+    });
+    gsap.to($cursorHover, {
+        x: e.clientX,
+        y: e.clientY,
+    });
+}
+
+// hover effect
+
+function cursorHover(e) {
+    gsap.to($cursor, {
+        scale: 0.5
+    });
+    gsap.to($cursorHover, {
+       scale: 3
+    });
+}
+
+// normal cursor
+function cursor(e) {
+    gsap.to($cursor, {
+        scale: 1
+    });
+    gsap.to($cursorHover, {
+       scale: 1
+    });
+}
+
+$(window).on('mousemove', cursorMover);
+
+
+
+$('.alert').hover(cursorHover, cursor);
